03984a49abeaffa6addb93757ffdf63b9e3b10e2
Lucas Monteiro <66444506+lucas-moont@users.noreply.github.com>
2022-06-12 19:34:07 -0300

    Close menu animation done. Also fixed a bug where menu mobile didn't show on a second click.

---
+++ scripts/menu.js
@@ -0,0 +1,40 @@
+let menuMobile = document.querySelector('.menuMobileWrap')
+let menuMobileUl = document.querySelector('.menuMobileWrap ul')
+let closeMenu = document.querySelector('.menuClose')
+let menuHamburg = document.querySelector('.menuHamburguer')
+
+function menuMobileAction(){
+    if (menuMobile.style.display == '' || menuMobile.style.display == 'none'){
+        console.log('Aqui')
+        menuMobile.style.display = 'block'
+        menuHamburg.style.opacity = '0'
+        menuHamburg.style.transform = 'translatex(-16px)'
+        setTimeout(() => {
+            menuMobile.style.opacity = '1'
+            menuMobile.style.transform = 'translatey(16px)'
+            menuMobileUl.style.pointerEvents = 'auto'
+            menuHamburg.style.display = 'none'
+            closeMenu.style.display = 'block'
+            setTimeout(() => {
+                closeMenu.style.opacity = 1
+                closeMenu.style.transform = 'translatex(0)'
+            }, 250)
+        }, 500);
+    }else if (menuMobile.style.display == 'block'){
+        menuMobile.style.opacity = '0'
+        menuMobile.style.zIndex = 4;
+        menuMobile.style.transform = 'translatey(0px)'
+        menuMobileUl.style.pointerEvents = 'auto'
+        closeMenu.style.opacity = '0'
+        closeMenu.style.transform = 'translatex(16px)'
+        setTimeout(() => {
+            menuMobile.style.display = 'none'
+            menuHamburg.style.display = 'block'
+            closeMenu.style.display = 'none'
+            setTimeout(() => {
+                menuHamburg.style.opacity = 1
+                menuHamburg.style.transform = 'translatex(0)'
+            }, 250)
+        }, 500);
+    }
+}
